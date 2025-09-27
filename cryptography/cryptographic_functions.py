b350801ebc992b507d31eee856eeb97518e23526
Nathaniel Puckett <napnathanielallenpuckett@gmail.com>
2025-09-26 20:11:31 -0400

    Minor fixes

--- cryptography/cryptographic_functions.py
+++ cryptography/cryptographic_functions.py
@@ -1,13 +1,11 @@
 import numpy as np
 
-from collections import Counter
-
 ALPHABET = [['E', 12.2], ['T', 8.8], ['A', 7.9], ['O', 7.2], ['I', 6.8], 
             ['N', 6.5], ['S', 6.1], ['H', 5.9], ['R', 5.8], ['D', 4.1], 
             ['L', 3.9], ['C', 2.7], ['U', 2.7], ['M', 2.3], ['W', 2.3], 
             ['F', 2.1], ['G', 1.9], ['Y', 1.9], ['P', 1.8], ['B', 1.4], 
             ['V', 1.0], ['K', 0.8], ['J', 0.2], ['X', 0.2], ['Z', 0.1],
-            ['Q', 0.1], ]
+            ['Q', 0.1]]
 
 def frequency_analysis(text:str):
     frequencies = dict()
@@ -24,7 +22,6 @@ def frequency_analysis(text:str):
 
     return frequencies
 
-
 def shift(mode:str, text:str, s:int):
     message = str()
     text = text.lower()
@@ -37,19 +34,16 @@ def shift(mode:str, text:str, s:int):
     
     return message
 
-
 def fa_shift(text:str, num_search:int): #uses frequency analysis on shift
     messages = str()
     frequencies = frequency_analysis(text)
 
     for frequency in frequencies[:num_search]:
         s = (ord(frequency[0]) - ord('A'))%26
-        messages += '\n'
-        messages += f'Shifted {str(s)} (A = {shift('e', 'A', s).upper()})'
+        messages += '\n' + f'Shifted {str(s)} (A = {shift('e', 'A', s).upper()})'
         messages += '\n' + shift('d', text, s) + '\n'
-    
-    return messages
 
+    return messages
 
 def affine(mode:str, text:str, a:int, b:int):
     message = str()
@@ -71,7 +65,6 @@ def affine(mode:str, text:str, a:int, b:int):
 
     return message
 
-
 def vigenere(mode:str, text:str, key:str):
     message = str()
     text = text.lower()
@@ -82,9 +75,8 @@ def vigenere(mode:str, text:str, key:str):
         k = key_vals[i % len(key)] * (1 if mode == 'e' else -1)
         e_char = (x+k)%26
         message += chr(e_char+97)
-    
-    return message
 
+    return message
 
 def hill(mode:str, text:str, matrix):
     message = str()
@@ -95,16 +87,14 @@ def hill(mode:str, text:str, matrix):
         det = int(round(np.linalg.det(matrix), 0))
         det_inv = pow(det, -1, 26)
         matrix = (np.linalg.inv(matrix)*det*det_inv)%26
-        print(matrix)
 
     for i in range(0, len(text), m):
         v_a = np.array([[ord(char)-97] for char in text[i:i+m]])
         v_b = np.matmul(matrix, v_a)
         for b in v_b:
             message += chr(int(round(b[0], 0))%26 + 97)
-    
-    return message
 
+    return message
 
 def permutation(mode:str, text:str, ordering:list):
     message = str()
@@ -120,14 +110,11 @@ def permutation(mode:str, text:str, ordering:list):
 
     for i in range(0, len(text), m):
         v_a = np.array([[ord(char)-97] for char in text[i:i+m]])
-        print(v_a)
-        print(ordering_matrix)
         v_b = np.matmul(ordering_matrix, v_a)
         for b in v_b:
             message += chr(int(round(b[0], 0))%26 + 97)
 
     return message
-
 
 def autokey(mode:str, text:str, key:int):
     message = str()
@@ -139,9 +126,8 @@ def autokey(mode:str, text:str, key:int):
         e_char = (x + (s if mode == 'e' else -s))%26
         message += chr(e_char+97)
         s = (x if mode == 'e' else e_char)
-    
-    return message
 
+    return message
 
 def bf_autokey(text:str): #uses brute force on autokey
     messages = str()
@@ -149,8 +135,6 @@ def bf_autokey(text:str): #uses brute force on autokey
 
     for key in range(26):
         attempt = autokey('d', text, key)
-        messages += '\n'
-        messages += f'{chr(key+97)} : {attempt}'
-        messages += '\n'
-    
+        messages += '\n' + f'{chr(key+97)} : {attempt}' + '\n'
+
     return messages
